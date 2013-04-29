3094e63f4b6d288450c63f0a9446666b59b83631
Dennis Ideler <ideler.dennis@gmail.com>
2013-04-29 06:43:24 -0400

    Update script

--- tmp.sh
+++ tmp.sh
@@ -4,7 +4,15 @@ if [ `git log --pretty=%H ...refs/heads/master^` != `git ls-remote origin -h ref
 then
   echo "Your branch has diverged from origin"
   git fetch --quiet
-  git log HEAD..origin/master --oneline
+  git log HEAD..origin/master --graph --abbrev-commit --date=relative \
+    --pretty=format:"%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset"
+fi
+
+if (`git push --quiet`)
+then
+  echo 'works'
+else
+  echo 'fails'
 fi
 
 if test -e .
